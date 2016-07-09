a5675ea5f00513c4facc9811740c6644a3b3aa54
Arunmozhi <arunmohzi@yahoo-inc.com>
2016-07-09 14:53:47 -0700

    Initial commit with Kafka consumer working

---
+++ src/main/java/com/yahoo/yamas/kafka/KafkaTopicReader.java
@@ -0,0 +1,71 @@
+package com.yahoo.yamas.kafka;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+import kafka.consumer.ConsumerConfig;
+import kafka.consumer.ConsumerIterator;
+import kafka.consumer.KafkaStream;
+import kafka.javaapi.consumer.ConsumerConnector;
+
+public class KafkaTopicReader {
+    private final ConsumerConnector consumer;
+    private final String topic;
+
+    public KafkaTopicReader(String zookeeper, String groupId, String topic) {
+        consumer = kafka.consumer.Consumer.createJavaConsumerConnector(createConsumerConfig(zookeeper, groupId));
+        this.topic = topic;
+    }
+
+    private static ConsumerConfig createConsumerConfig(String zookeeper, String groupId) {
+        Properties props = new Properties();
+        props.put("zookeeper.connect", zookeeper);
+        props.put("group.id", groupId);
+        props.put("zookeeper.session.timeout.ms", "1200000");
+        props.put("zookeeper.connection.timeout.ms", "1200000");
+        props.put("rebalance.backoff.ms", "60000");
+        props.put("rebalance.retries.max", "20");
+        props.put("auto.commit.enable", "true");
+        props.put("auto.offset.reset", "smallest");
+
+        return new ConsumerConfig(props);
+
+    }
+
+    public void testConsumer() {
+        // TopicFilter topicfilter;
+        Map<String, Integer> topicMap = new HashMap<String, Integer>();
+
+        // Define single thread for topic
+        topicMap.put(topic, new Integer(1));
+
+        Map<String, List<KafkaStream<byte[], byte[]>>> consumerStreamsMap = consumer.createMessageStreams(topicMap);
+
+        List<KafkaStream<byte[], byte[]>> streamList = consumerStreamsMap.get(topic);
+        int count = 0;
+        for (final KafkaStream<byte[], byte[]> stream : streamList) {
+            ConsumerIterator<byte[], byte[]> consumerIte = stream.iterator();
+            while (consumerIte.hasNext()) {
+                System.out.println("Message from Single Topic :: " + new String(consumerIte.next().message()));
+                count++;
+                // stop after 3 messages read
+                if (count > 2)
+                    break;
+            }
+        }
+        if (consumer != null)
+            consumer.shutdown();
+    }
+
+    public static void main(String[] args) {
+
+        String zooKeeper = args[0];
+        String groupId = args[1];
+        String topic = args[2];
+        KafkaTopicReader simpleHLConsumer = new KafkaTopicReader(zooKeeper, groupId, topic);
+        simpleHLConsumer.testConsumer();
+    }
+
+}
---
+++ src/main/java/com/yahoo/yamas/storm/FirstTopology.java
@@ -0,0 +1,36 @@
+package com.yahoo.yamas.storm;
+
+
+import backtype.storm.Config;
+import backtype.storm.LocalCluster;
+import backtype.storm.StormSubmitter;
+import backtype.storm.topology.TopologyBuilder;
+import backtype.storm.utils.Utils;
+
+public class FirstTopology {
+
+    public static void main(String[] args) throws Exception {
+        TopologyBuilder builder = new TopologyBuilder();
+
+        builder.setSpout("firstSpout", new FirstSpout(), 2);
+        builder.setBolt("firstBolt", new FirstBolt(), 3).shuffleGrouping("firstSpout");
+
+        Config conf = new Config();
+        conf.setDebug(true);
+
+        if (args != null && args.length > 0) {
+            conf.setNumWorkers(20);
+            conf.setDebug(true);
+            StormSubmitter.submitTopology(args[0], conf, builder.createTopology());
+        } else {
+
+            LocalCluster cluster = new LocalCluster();
+            cluster.submitTopology("test", conf, builder.createTopology());
+            Utils.sleep(10000);
+            cluster.killTopology("test");
+            cluster.shutdown();
+        }
+
+    }
+
+}
